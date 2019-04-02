ebfe4d71f5b30bcc2d20ff6e144ac226d5ada129
Chiara Relevat <rc804998@iutnice.unice.fr>
2019-04-02 17:51:13 +0200

    adaptation du main a l'ajout de la classe position

--- src/visuel/main.java
+++ src/visuel/main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 import joueurs.Joueur;
 
 import piecesEchiquier.Echiquier;
+import piecesEchiquier.Position;
 
 public class main {
 
@@ -13,22 +14,25 @@ public class main {
 		//ouvre une porte pour input
 		Scanner sc = new Scanner(System.in);
 		
-		
 		// variable tempo pour tests
 		boolean jeu = true;
 		
-		
+		//position de la piece choisie et l'emplacement ou la poser
+		Position p1 = new Position();
+		Position p2 = new Position();
 		// joueurs APRES ON DEMANDERA NOM
 		Joueur j1 = new Joueur("Manon","Blanc");
 		Joueur j2 = new Joueur("Autre","Noir");
 		
 		
 		// vars pour deplacer
+		/*
 		int x1;
 		int y1;
 		int x2;
 		int y2;
-		
+		*/
+		String dep;
 		
 		
 		System.out.println("Bienvenue dans ce jeu d'echec sur console !\n");
@@ -44,33 +48,39 @@ public class main {
 				// JOUEUR1
 				System.out.println("Tour de "+j1.getNom()+"\n");
 				
-				System.out.println("Quelle pièce voulez vous déplacer ? Donnez le numero de ligne puis de colonne.\n");
+				do
+				{
+				System.out.println("Quelle piÃ¨ce voulez vous dÃ©placer ? Donnez la combinaison lettre/chiffre.\n");
+				String initial = sc.nextLine();
+				p1 = e.traduction(initial);
+				}while (p1 ==  null);
 				
-				//entrer  coordonnées piece a bouger APRES ON PASSERA PAR DIRECT COMBINAISON LETTRE-CHIFFRE
-				x1 = sc.nextInt();
-				y1 = sc.nextInt();
+				do
+				{
+				System.out.println("A quel endroit la poser ? Donnez la combinaison lettre/chiffre.\\n");
+				//entrer  coordonnÃ©es endroit visÃ© APRES ON PASSERA PAR DIRECT COMBINAISON LETTRE-CHIFFRE
 				
-				System.out.println("A quel endroit la poser ? Donnez le numero de ligne puis de colonne.\n");
-				//entrer  coordonnées endroit visé APRES ON PASSERA PAR DIRECT COMBINAISON LETTRE-CHIFFRE
-				x2 = sc.nextInt();
-				y2 = sc.nextInt();
+				String newemp = sc.nextLine();
+				p2 = e.traduction(newemp);
+				}while (p2 ==  null);
 				
 				// DEPLACEMENT (prototype minimum juste pour voir)
-				e.deplacer(x1-1,y1-1,x2-1,y2-1);
+				e.deplacer(p1,p2);
 				
 				//REAFFICHAGE ECHIQUIER
 				System.out.println(e.toString());
 				
 				// JOUEUR2
+				/*
 				System.out.println("Tour de"+j2.getNom()+"\n");
-				System.out.println("Quelle pièce voulez vous déplacer ? Donnez le numero de ligne puis de colonne.\n");
+				System.out.println("Quelle piÃ¨ce voulez vous dÃ©placer ? Donnez le numero de ligne puis de colonne.\n");
 				x1 = sc.nextInt();
 				y1 = sc.nextInt();
 				System.out.println("A quel endroit la poser ? Donnez le numero de ligne puis de colonne.\n");
 				x2 = sc.nextInt();
 				y2 = sc.nextInt();
 				e.deplacer(x1-1,y1-1,x2-1,y2-1);
-
+*/
 			//jeu = false;
 			 
 		}while (jeu == true);	
