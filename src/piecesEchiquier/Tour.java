8f6df580b0662d63afc4f0f5b738015c51872aa8
Jose Srifi <jose.srifi@gmail.com>
2019-05-09 10:27:45 +0200

    update Tour.java
    suppression méthode aBouge() car présente dansclasse parente.

--- src/piecesEchiquier/Tour.java
+++ src/piecesEchiquier/Tour.java
@@ -1,24 +1,10 @@
 package piecesEchiquier;
 
 public class Tour extends Piece {
-	private boolean verif=false;
 	
 	public Tour(String couleur,String l, Position pos)
 	{
 		super(couleur,l,pos);
-	}
-	
-	public boolean aBouge()
-	{
-		if(this.getIni() != this.getPosition())
-		{
-			verif=true;
-		}
-		if(this.getIni() == this.getPosition() && verif == false)
-		{
-			verif=false;
-		}
-		return verif;
 	}
 	
 	public boolean[][] deplacable(Echiquier e,Piece p) {
