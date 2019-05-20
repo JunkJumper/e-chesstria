5f0f79a7a8ffa331132ddd5aba4ee5fc3547b0d6
Jose Srifi <jose.srifi@gmail.com>
2019-05-20 09:53:59 +0200

    ajout choix configuration partie
    - choix des noms de joueurs
    - choix manuel de qui prends les noirs et qui prends les blancs
    - choix aléatoire de qui prends les noirs et qui prends les blancs

--- src/visuel/main.java
+++ src/visuel/main.java
@@ -7,6 +7,7 @@ import joueurs.Joueur;
 import piecesEchiquier.Echiquier;
 
 public class main {
+	static int while1 = 1;
 
 	public static void main (String[] args) {
 		
@@ -20,10 +21,53 @@ public class main {
 		// joueurs
 		
 		System.out.println("Entrez le nom du joueur 1 (blanc) : ");
-		Joueur j1 = new Joueur(sc.nextLine(),"Blanc");
+		Joueur j1 = new Joueur(sc.nextLine(), null);
 		
 		System.out.println("Entrez le nom du joueur 2 (noir) : ");
-		Joueur j2 = new Joueur(sc.nextLine(),"Noir");
+		Joueur j2 = new Joueur(sc.nextLine(),null);
+		
+		while(while1 == 1) {
+		
+		System.out.println("Choisissez la configuration de partie que vous désirez en tapant le numéro correspondant :");
+		System.out.println("1 = Blanc : " + j1.getNom() + " - Noir : " + j2.getNom());
+		System.out.println("2 = Blanc : " + j2.getNom() + " - Noir : " + j1.getNom());
+		System.out.println("3 = Aléatoire");
+		
+		String choixP = sc.nextLine();
+		
+		if(choixP.equalsIgnoreCase("3"))
+		{
+			int random = (int)(Math.random() * 2 + 1);
+			
+			System.out.println("L'aléatoire a été effectué, la valeur du choix est = " + random);
+			
+			if(random <=1)
+			{
+				choixP = "1";
+			} else {
+				choixP = "2";
+			}
+		}
+		
+		if(choixP.equalsIgnoreCase("1"))
+		{
+			j1.setCouleur("Blanc");
+			j2.setCouleur("Noir");
+			System.out.println("Le joueur " + j1.getNom() + " jouera les blanc et le joueur " + j2.getNom() + " jouera les noirs.");
+			while1 = 2;
+			
+		} else if(choixP.equalsIgnoreCase("2"))
+		{
+			j2.setCouleur("Blanc");
+			j1.setCouleur("Noir");
+			System.out.println("Le joueur " + j2.getNom() + " jouera les blanc et le joueur " + j1.getNom() + " jouera les noirs.");
+			while1 = 2;
+			
+		} else
+		{
+			System.err.println("Veuillez rentrer une valeur correcte");
+		}
+		}
 		
 		// vars pour deplacer
 		String A;
