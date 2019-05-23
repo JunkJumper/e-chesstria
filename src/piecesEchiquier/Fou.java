5255f9932ba558158123e899f9d3ac2a078082cf
Manon <manon.thebault@etu.unice.fr>
2019-05-23 17:35:58 +0200

    Fou refait

--- src/piecesEchiquier/Fou.java
+++ src/piecesEchiquier/Fou.java
@@ -9,83 +9,66 @@ public class Fou extends Piece {
 	
 	public boolean deplacable(Echiquier e,Piece p)
 	{
-		if (e.estVide(p.getPosition()) == false) //si on mange une piece
-			System.out.println("Vous avez mangé une pièce !");
+		//if (e.estVide(p.getPosition()) == false) //si on mange une piece
+		//	System.out.println("Vous avez mangé une pièce !");
 		
-		for (int i = 1; i < 8; i++)				// ON CHERCHE SI LA 2EME COORS EST EN DIAGONALE DE LA 1 ERE
+		if (p.getPosition().getX() < this.getPosition().getX())
 		{
-
-			if ( p.getPosition().getX() == this.getPosition().getX()+i)		// 2eme coors a droite de la 1 ere
+			if (this.getPosition().getX() - p.getPosition().getX() == p.getPosition().getY()-this.getPosition().getY())
 			{
-				
-				//DEPLACEMENT HAUT DROITE
-				if (p.getPosition().getY() == this.getPosition().getY()+i )	// on teste si quand on avance de x vers la droite on monte aussi de x
+				for (int i = 1; i < p.getPosition().getY()-this.getPosition().getY();i++)
 				{
-					for (int j = 1;this.getPosition().getX()+j <= p.getPosition().getX()-1;j++) // on verifie que le chemin est libre
+					if ( !(e.getEchiquier()[8-this.getPosition().getY()-i][this.getPosition().getX()-i-1].getNom().equals(".."))   )
 					{
-						if ( !(e.getEchiquier()[8-(this.getPosition().getY()+j)][this.getPosition().getX()+j-1].getNom().equals(".."))   )
-						{
-							System.err.println("Le chemin n'est pas libre.");
-							return false;
-						}
+						System.err.println("Le chemin n'est pas libre.");
+						return false;
 					}
-					return true;
-				}
-				
-				//DEPLACEMENT BAS DROITE
-				else if (p.getPosition().getY() == this.getPosition().getY()-i)  // on teste si quand on avance de x vers la droite on descend aussi de x
-				{
-					for (int j = 1;this.getPosition().getX()+j <= p.getPosition().getX()-1;j++) 	// on verifie que le chemin est libre
-					{
-						if ( !(e.getEchiquier()[8-(this.getPosition().getY()-j)][this.getPosition().getX()+j-1].getNom().equals(".."))   )
-						{
-							System.err.println("Le chemin n'est pas libre.");
-							return false;
-						}
-					}
-					return true;
-				}
-				
-				
-				
-			}
- 
-			else if ( p.getPosition().getX() == this.getPosition().getX()-i)	// 2eme coors a gauche de la 1 ere
-			{
-				
-				//DEPLACEMENT HAUT GAUCHE
-				if (p.getPosition().getY() == this.getPosition().getY()+i )	// on teste si quand on va en x vers la gauche on monte aussi de x
-				{
-					for (int j = 1;this.getPosition().getX()-j >= p.getPosition().getX()+1;j++) // on verifie que le chemin est libre
-					{
-						if ( !(e.getEchiquier()[8-(this.getPosition().getY()+j)][this.getPosition().getX()+j-1].getNom().equals(".."))   )
-						{
-							System.out.println("Le chemin n'est pas libre.");
-							return false;
-						}
-					}
-					return true;
-				}
-				
-				//DEPLACEMENT BAS DROITE
-				else if (p.getPosition().getY() == this.getPosition().getY()-i)  // on teste si quand on va de x vers la droite on descend aussi de x
-				{
-					for (int j = 1;this.getPosition().getX()-j >= p.getPosition().getX()+1;j++) 	// on verifie que le chemin est libre
-					{
-						if ( !(e.getEchiquier()[8-(this.getPosition().getY()-j)][this.getPosition().getX()+j-1].getNom().equals(".."))   )
-						{
-							System.out.println("Le chemin n'est pas libre.");
-							return false;
-						}
-					}
-					return true;
 				}
 				return true;
 			}
+			else if (this.getPosition().getX() - p.getPosition().getX() == this.getPosition().getY()-p.getPosition().getY())
+			{
+				for (int i = 1; i < this.getPosition().getY()-p.getPosition().getY();i++)
+				{
+					if ( !(e.getEchiquier()[8-this.getPosition().getY()+i][this.getPosition().getX()-i-1].getNom().equals(".."))   )
+					{
+						System.err.println("Le chemin n'est pas libre.");
+						return false;
+					}
+				}
+				return true;
+			}
+		}else if (p.getPosition().getX() > this.getPosition().getX())
+		{
+			if (p.getPosition().getX() - this.getPosition().getX() == p.getPosition().getY()-this.getPosition().getY())
+			{
+				for (int i = 1; i < p.getPosition().getY()-this.getPosition().getY();i++)
+				{
+					if ( !(e.getEchiquier()[8-this.getPosition().getY()-i][this.getPosition().getX()+i-1].getNom().equals(".."))   )
+					{
+						System.err.println("Le chemin n'est pas libre.");
+						return false;
+					}
+				}
+				return true;
+			}
+			else if (p.getPosition().getX() - this.getPosition().getX() == this.getPosition().getY()-p.getPosition().getY())
+			{
+				for (int i = 1; i < this.getPosition().getY()-p.getPosition().getY();i++)
+				{
+					if ( !(e.getEchiquier()[8-this.getPosition().getY()+i][this.getPosition().getX()+i-1].getNom().equals(".."))   )
+					{
+						System.err.println("Le chemin n'est pas libre.");
+						return false;
+					}
+				}
+				return true;
+			}
+			
 		}
 		return false;
-		
 	}
 	
 	
 }
+
